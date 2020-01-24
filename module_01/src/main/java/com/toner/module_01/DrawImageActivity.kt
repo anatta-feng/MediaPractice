eddb93c35ab0c19732209903fc2c8d34eb5f1a78
T-Oner <1@fxcdev.com>
2020-01-24 10:15:56 +0800

    Add 01 module

---
+++ module_01/src/main/java/com/toner/module_01/DrawImageActivity.kt
@@ -0,0 +1,12 @@
+package com.toner.module_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_image)
+    }
+}
