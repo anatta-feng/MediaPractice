74255216527ab657ed5c6b1750e1132aa757f4cf
T-Oner <1@fxcdev.com>
2020-01-31 21:27:43 +0800

    Add module_04 for MediaExtractor

---
+++ module_04/src/main/java/com/toner/module_04/MediaExtractorActivity.kt
@@ -0,0 +1,12 @@
+package com.toner.module_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaExtractorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_extractor)
+    }
+}
