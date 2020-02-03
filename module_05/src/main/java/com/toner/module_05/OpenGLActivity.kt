fe02d40bfd3f82cd033a644145622c467828956a
T-Oner <1@fxcdev.com>
2020-02-03 21:59:54 +0800

    Add module 05

---
+++ module_05/src/main/java/com/toner/module_05/OpenGLActivity.kt
@@ -0,0 +1,12 @@
+package com.toner.module_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenGLActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_gl)
+    }
+}
